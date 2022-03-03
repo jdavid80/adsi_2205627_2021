004657abc795230a54be8426f422b6c493716e75
JDavid80 <jdcalderong@gmail.com>
2022-03-03 15:52:58 -0500

    add tabla-productos

---
+++ javascript/prueba/js/script.js
@@ -0,0 +1,5 @@
+function crearTabla() {
+    let contenedor = document.getElementById("main-content");
+    let nodeToAddTable = document.createElement("table");
+    contenedor.appendChild(nodeToAddTable);
+}
